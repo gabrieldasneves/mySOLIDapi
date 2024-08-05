90523cd14540f17604e73c3700e27216592a706b
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-08-05 23:16:54 +0900

    feat: installing bcryptjs to work with hash for password

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { hash } from "bcryptjs";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -10,12 +11,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   });
 
   const { name, email, password } = registerBodySchema.parse(request.body);
-
+  const passwordHash = await hash(password, 5);
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash: passwordHash,
     },
   });
 
